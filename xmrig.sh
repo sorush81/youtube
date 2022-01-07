3f9c47b5e2253f0f0a365f496a57c1c1583a9456
sorush81 <94933887+sorush81@users.noreply.github.com>
2022-01-07 13:19:50 +0330

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -2,7 +2,7 @@
  wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz
 tar xf xmrig-6.15.1-linux-x64.tar.gz
 cd xmrig-6.15.1
-./xmrig -o de.turtlecoin.herominers.com:1160 -u TRTLv3ddBegcU6QmjSPeyHAX5C14vE1dZaNFFnXcs8qr2U8f7Ypnbkx3Uhb6ZTsrqA63gkoAJZ1ZLhwooN9NhaTYDVrZeAaA6A4 -p zero -k -a argon2/chukwav2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u USDT:TYf7SMWvU3YqAWK7p848TdeaYC4jGUvxqd.test1 -p x pause
 while [ 1 ]; do
 sleep 3
 done
